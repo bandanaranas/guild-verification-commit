ca8cbfe08b510acff87ccba99629169c52a2c519
bandanaranas <87375265+bandanaranas@users.noreply.github.com>
2025-11-10 03:18:39 +0300

    Fix memory leak in event handlers

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-09T21:48:16.924Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2025-11-10T00:18:39.560Z
+Commit: Fix memory leak in event handlers
