77cd9ced286509d92f7bba1d3cb51cb073266dce
bandanaranas <87375265+bandanaranas@users.noreply.github.com>
2025-11-12 23:13:34 +0300

    Fix race condition in async operations

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-12T20:13:21.283Z
-Commit: Implement user profile customization
+Timestamp: 2025-11-12T20:13:34.513Z
+Commit: Fix race condition in async operations
