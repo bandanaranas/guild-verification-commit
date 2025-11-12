9096fdab4f4ab79781e4eff5b7e2a3122b815d36
bandanaranas <87375265+bandanaranas@users.noreply.github.com>
2025-11-12 19:15:55 +0300

    Fix broken links in documentation

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-12T07:27:54.275Z
-Commit: Correct typo in configuration file
+Timestamp: 2025-11-12T16:15:55.390Z
+Commit: Fix broken links in documentation
