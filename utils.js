e537234ab77616a6bbea18ad37f51c5e6b6279a3
bandanaranas <87375265+bandanaranas@users.noreply.github.com>
2025-11-12 19:15:50 +0300

    Fix memory leak in event handlers

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-12T07:28:05.053Z
-Commit: Fix broken links in documentation
+Timestamp: 2025-11-12T16:15:50.525Z
+Commit: Fix memory leak in event handlers
