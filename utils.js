21ee6114ec4d8fbdd8261356fcf00ca27ced0759
bandanaranas <87375265+bandanaranas@users.noreply.github.com>
2025-11-12 10:28:05 +0300

    Fix broken links in documentation

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-12T07:28:00.531Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2025-11-12T07:28:05.053Z
+Commit: Fix broken links in documentation
