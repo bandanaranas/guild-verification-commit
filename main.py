2f0bc45ae8e2368b8fbdc19a52b313f91a4b61ab
bandanaranas <87375265+bandanaranas@users.noreply.github.com>
2025-11-14 00:03:05 +0300

    Fix memory leak in event handlers

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-13T21:02:56.564Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2025-11-13T21:03:04.515Z
+Commit: Fix memory leak in event handlers
