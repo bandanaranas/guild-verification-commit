fe914fc35173c87ae4525d67407bf5039c1fceac
bandanaranas <87375265+bandanaranas@users.noreply.github.com>
2025-11-17 02:45:28 +0300

    Add multi-language support

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-16T23:45:24.429Z
-Commit: Patch security vulnerability in authentication
+Timestamp: 2025-11-16T23:45:27.677Z
+Commit: Add multi-language support
