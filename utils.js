a1bba32020bc82de8916764af1f54ebbc69f9239
bandanaranas <87375265+bandanaranas@users.noreply.github.com>
2025-11-17 02:45:27 +0300

    Resolve null pointer exception in user service

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-16T08:46:33.026Z
-Commit: Implement user profile customization
+Timestamp: 2025-11-16T23:45:25.893Z
+Commit: Resolve null pointer exception in user service
