39d5dc30daa06ba1533510a47df4714bca51c9e2
bandanaranas <87375265+bandanaranas@users.noreply.github.com>
2025-11-23 21:33:30 +0300

    Fix broken links in documentation

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-23T18:33:20.223Z
-Commit: Correct typo in configuration file
+Timestamp: 2025-11-23T18:33:29.508Z
+Commit: Fix broken links in documentation
