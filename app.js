7d3f7f286a57c8003b4c84946ddd08c8250e30d1
bandanaranas <87375265+bandanaranas@users.noreply.github.com>
2025-11-27 07:06:34 +0300

    Resolve timezone issues in date handling

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-25T20:47:12.441Z
-Commit: Add multi-language support
+Timestamp: 2025-11-27T04:06:34.813Z
+Commit: Resolve timezone issues in date handling
