d992873b0a86bb7dd7e72499d2e3c7409fd002b0
bandanaranas <87375265+bandanaranas@users.noreply.github.com>
2025-11-27 23:47:32 +0300

    Patch security vulnerability in authentication

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-25T20:47:18.281Z
-Commit: Add export functionality for reports
+Timestamp: 2025-11-27T20:47:31.987Z
+Commit: Patch security vulnerability in authentication
