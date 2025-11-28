89beb3e264ab9ca4f14cae69c35402755dd54460
bandanaranas <87375265+bandanaranas@users.noreply.github.com>
2025-11-28 09:55:41 +0300

    Resolve null pointer exception in user service

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-27T20:47:31.987Z
-Commit: Patch security vulnerability in authentication
+Timestamp: 2025-11-28T06:55:40.047Z
+Commit: Resolve null pointer exception in user service
