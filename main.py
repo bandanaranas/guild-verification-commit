768054013b21c4923d8438a3fbdd354c55c1b32b
bandanaranas <87375265+bandanaranas@users.noreply.github.com>
2025-12-02 21:16:20 +0300

    Patch API endpoint error handling

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-30T20:43:09.653Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2025-12-02T18:16:20.002Z
+Commit: Patch API endpoint error handling
