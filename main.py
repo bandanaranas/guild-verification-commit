a132061cb501b46904ac977d34ff8589d116783a
bandanaranas <87375265+bandanaranas@users.noreply.github.com>
2025-12-04 11:25:21 +0300

    Fix memory leak in event handlers

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-12-04T08:25:12.562Z
-Commit: Correct typo in configuration file
+Timestamp: 2025-12-04T08:25:19.897Z
+Commit: Fix memory leak in event handlers
